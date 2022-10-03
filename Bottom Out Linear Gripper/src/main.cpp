d41a77c22277244c66f7ae58c0a90bd1fdabb848
Max Berman <32885668+maximus-lazer@users.noreply.github.com>
2022-10-03 10:47:54 -0400

    Linear Actuator Code

---
+++ Bottom Out Linear Gripper/src/main.cpp
@@ -0,0 +1,142 @@
+#include <Arduino.h>
+#include <Romi32U4.h>
+#include "Timer.h"
+#include <servo32u4.h>
+Romi32U4ButtonB buttonB;
+
+/*
+The FS90R continuous rotation servo converts 
+standard RC servo position pulses into continuous
+rotation speed. The default rest point is 1.5 ms, but
+this can be adjusted by using a small screwdriver
+to turn the middle-point adjustment potentiometer.
+Pulse widths above the rest point result in counterclockwise 
+rotation, with speed increasing as the pulse width increases;
+pulse widths below the rest point result in clockwise rotation, 
+with speed increasing as the pulse width decreases.
+*/
+
+//int servoPin = 5;
+int linearPotPin = 18;
+int servoStop = 1500;  
+int servoJawDown = 1000;  
+int servoJawUp = 2000;  
+int printDelay = 500;
+int linearPotVoltageADCOLD = 0;
+int linearPotVoltageADC = 500;
+int linearPotVoltageADCTolerance = 10;
+int jawOpenPotVoltageADC = 300; // was 600
+int jawClosedPotVoltageADC = 1100; // was 940
+Romi32U4ButtonA buttonA;
+
+enum gripperStateChoices{OPEN, CLOSE, STOP} gripperState, nextGripperState;
+
+
+Servo32U4 jawServo;
+
+Timer printTimer(printDelay);
+//timenow - time increases
+//oldtime - last time it checked
+
+void setup()
+{
+  Serial.begin(9600);
+  jawServo.attach();
+}
+
+void openGripper() {
+  jawServo.writeMicroseconds(servoJawDown);
+  Serial.print("linearPotVoltageADCJawDOWN:     ");
+       Serial.println(linearPotVoltageADC);
+  linearPotVoltageADC = analogRead(linearPotPin);
+
+  while (linearPotVoltageADC > jawOpenPotVoltageADC) {
+         Serial.print("linearPotVoltageADCJawDOWNINSIDE:     ");
+      Serial.println(linearPotVoltageADC);
+      linearPotVoltageADC = analogRead(linearPotPin);
+    if(linearPotVoltageADC == linearPotVoltageADCOLD) {
+      linearPotVoltageADCOLD = analogRead(linearPotPin);
+ 
+    }
+
+  }
+}
+
+void closeGripper() {
+ jawServo.writeMicroseconds(servoJawUp);
+  Serial.print("linearPotVoltageADCJawUP:     ");
+       Serial.println(linearPotVoltageADC);
+  linearPotVoltageADC = analogRead(linearPotPin);
+
+  while (linearPotVoltageADC < jawClosedPotVoltageADC) {
+     Serial.print("linearPotVoltageADCJawUPINSIDE:     ");
+       Serial.println(linearPotVoltageADC);
+    linearPotVoltageADC = analogRead(linearPotPin);
+  }
+}
+
+void setGripperState(gripperStateChoices STATE) {
+  switch(STATE) {
+
+    linearPotVoltageADC = analogRead(linearPotPin);
+
+    case OPEN:
+
+      jawServo.writeMicroseconds(servoJawDown);
+
+      if (linearPotVoltageADC > jawOpenPotVoltageADC) {
+        gripperState = STOP;
+        nextGripperState = CLOSE;
+        setGripperState(gripperState);
+      } else {
+        setGripperState(OPEN);
+      }
+
+    break;
+
+    case CLOSE:
+
+      jawServo.writeMicroseconds(servoJawUp);
+
+      // Checks to see if gripper has finished closeing, if yes then stop
+      if (linearPotVoltageADC < jawClosedPotVoltageADC) {
+        gripperState = STOP;
+        nextGripperState = OPEN;
+        setGripperState(gripperState);
+
+      // Checks to see if gripper is jammed, if it is then stop the gripper
+      } else if (abs(linearPotVoltageADCOLD - linearPotVoltageADC) < linearPotVoltageADCTolerance) {
+        gripperState = STOP;
+        nextGripperState = CLOSE;
+        setGripperState(gripperState);
+
+      // Keeps closing if gripper hasn't stopped
+      } else {
+        setGripperState(CLOSE);
+      }
+
+      linearPotVoltageADCOLD = linearPotVoltageADC; // Sets old linPotVoltage to previous
+
+    break;
+
+    case STOP:
+
+      jawServo.writeMicroseconds(servoStop);
+
+    break;
+  }
+
+}
+
+void loop()
+{
+  
+  if (buttonB.isPressed()) {
+    setGripperState(OPEN);
+  }
+  
+  if (buttonA.isPressed()) {
+    setGripperState(CLOSE);
+  }
+
+}
